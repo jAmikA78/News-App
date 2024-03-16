bc831f279bcd4ade047ef1d1c2226db1bbfc086c
jamika78 <ahmed.ibrahim7893@gmail.com>
2024-03-16 06:17:23 +0200

    first commit

---
+++ lib/views/homePage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class firstScreen extends StatelessWidget {
+  const firstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
