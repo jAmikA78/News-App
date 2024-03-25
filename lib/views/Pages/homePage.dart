94af4e5a9e75e58d7cc1c793018865d1d3862691
jamika78 <ahmed.ibrahim7893@gmail.com>
2024-03-25 14:35:20 +0200

    Add NewsCardList to homePage body
    Add NewsCardList widget to the homePage body within a CustomScrollView for better layout.

--- lib/views/Pages/homePage.dart
+++ lib/views/Pages/homePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/views/Widgets/CategoryCardList.dart';
+import 'package:newsapp/views/Widgets/NewsCardList.dart';
 
 class homePage extends StatelessWidget {
   const homePage({super.key});
@@ -21,7 +22,30 @@ class homePage extends StatelessWidget {
           ],
         ),
       ),
-      body: CategoryCardList(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(
+              child: CategoryCardList(),
+            ),
+            const SliverToBoxAdapter(
+              child: SizedBox(height: 22),
+            ),
+            const SliverToBoxAdapter(
+              child: NewsCardList(),
+            ),
+          ],
+        ),
+        // Column(
+        //   children: [
+        //     CategoryCardList(),
+        //     const SizedBox(height: 22),
+        //     const Expanded(child: NewsCardList()),
+        //   ],
+        // ),
+      ),
     );
   }
 }
